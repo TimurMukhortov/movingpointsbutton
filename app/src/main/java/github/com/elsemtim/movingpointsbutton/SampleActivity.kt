637687b72cb5c9a94cb46ff69e7fbe8ff18aec57
timur.mukhortov <t.mukhortov@gmail.com>
2018-06-29 16:57:23 +0300

    First commit.

---
+++ app/src/main/java/github/com/elsemtim/movingpointsbutton/SampleActivity.kt
@@ -0,0 +1,12 @@
+package github.com.elsemtim.movingpointsbutton
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample)
+    }
+}
